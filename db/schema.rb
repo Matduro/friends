b1358bdd5fa5cea3275b2ef84f627ecfe5bfbb8b
Mathieu <mathieurousseau123@gmail.com>
2021-08-03 19:38:10 -0700

    Update schema with user ID

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_03_165426) do
+ActiveRecord::Schema.define(version: 2021_08_04_022711) do
 
   create_table "friends", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
     t.string "email"
-    t.string "phone"
+    t.string "phone" 
     t.string "twitter"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_friends_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
